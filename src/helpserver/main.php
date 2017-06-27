77e30130f3e2a1c6a6991007220274bcc91fa31b
plugmcpe <M7mmdgamer7@icloud.com>
2017-06-27 11:58:03 +0300

    Update main.php

--- src/helpserver/main.php
+++ src/helpserver/main.php
@@ -12,6 +12,24 @@ public function onDisable(){
 $this->getServer()->getLogger()->info("plugin helpserver disable !");
 
  }
+pubilc function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+switch($cmd->getName()){
+ case 'power':
+  $sender->setHealth(100);
+  break;
+ case 'broadcast':
+    $this->getServer()->broadcast($args[0]);
+  break;
+ case 'gm1':
+  $player = $sender->getPlayer();
+  $player->setGamemode(1);
+  break;
+ case 'gm0':
+  $player = $sender->getPlayer();
+  $player->setGamemode(0);
+  break;
+}  
+ }
 }
 
 
